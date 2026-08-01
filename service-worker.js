eae6e979361fdc33b391f02692651f6493bc1d18
Josehugo <josehugo.tec@gmail.com>
2026-08-01 10:36:14 -0500

    actualizacion 3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE='rutas-inspeccion-v2';
-const CORE=['./','./index.html','./404.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil((async()=>{
+  const keys = await caches.keys();
+  await Promise.all(keys.map(key => caches.delete(key)));
+  await self.clients.claim();
+})()));
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
