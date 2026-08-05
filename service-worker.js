235b616e1a434b70b97730b2180b8cf87f9c973d
Josehugo <josehugo.tec@gmail.com>
2026-08-05 10:47:21 -0500

    act40

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ri-v13.31';
-const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png'];
+const CACHE='ri-v13.40';
+const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./login-hero-jh.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
